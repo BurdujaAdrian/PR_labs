d323c87e039b43d1bebe7bce9835d5984c1acb40
BurdujaAdrian <adrianburduja7.3@gmail.com>
2025-10-11 17:55:55 +0300

    added file_map, implemented counter, printing for counter, file caching

--- lab2/main.py
+++ lab2/main.py
@@ -1,8 +1,21 @@
 import socket
 import os
-
+from dataclasses import dataclass
 from concurrent.futures import ThreadPoolExecutor
 
+
+@dataclass
+class FileRecord:
+    data:bytes
+    counter:int
+
+
+client_map:dict[tuple[str, int],int] = {}
+file_map:dict[str,FileRecord] = {}
+
+def print_files(map_of_files:dict[str,FileRecord]):
+    for keys,value in map_of_files.keys():
+        print(f"keys:{keys}, value:{value}")
 
 def not_found():
     return "HTTP/1.1 404 Not Found\r\n\r\n"
@@ -22,9 +35,16 @@ def ok_html(data:str)->str:
 
 
 def respond_dir(args: list[str]) -> str:
+
+
     path = "./"
     for a in args:
         path += a + "/"
+
+    if path in file_map:
+        file_map[path].counter +=1
+    else:
+        file_map[path] = FileRecord(b"",1)
 
     files = []
     try:
@@ -36,18 +56,33 @@ def respond_dir(args: list[str]) -> str:
     
     html_content  =  "<html><head><title>Lab1</title></head><body>"
     html_content += f"<h1>Directory Listing for {path}</h1><ul>"
-    
+    html_content += "<table border='1' >"
+    html_content += "<tr><th>File/Directory</th><th>Hits</th></tr>"
     html_content += "<hr>"
     for file in files:
+
         if os.path.isdir(os.path.join(path, file)):
-            html_content += f'<li><a href="{file}/">{file}/</a></li>'
+            file_path = f"{path}{file}/"
+            if file_path not in file_map:
+                file_map[file_path] = FileRecord(b"",0)
+            
+            entry = file_map[file_path]
+            html_content += f'<tr><td><a href="{file}/">{file}/</a> </td><td>{entry.counter}</a></td></tr>'
         else:
-            html_content += f'<li><a href="{file}">{file}</a></li>'
+            
+            file_path = f"{path}{file}"
+            if file_path not in file_map:
+                file_map[file_path] = FileRecord(b"",0)
+            
+            entry = file_map[file_path]
+            html_content += f'<tr><td><a href="{file}">{file}</a> </td><td> {entry.counter}</a></td></tr>'
     
+    html_content += "</table>"
     html_content += "<hr>"
-    html_content += "</ul></body></html>"
+    html_content += "</body></html>"
     
     return ok_html(html_content)
+
 
 
 def respond_file(args:list[str])->tuple[str,bytes]:
@@ -55,6 +90,7 @@ def respond_file(args:list[str])->tuple[str,bytes]:
     path = "./"+args[0]
     for a in args[1:]:
         path += "/"+a
+
 
     content_type = ""
     if path.split(".")[-1] == "html":
@@ -66,12 +102,21 @@ def respond_file(args:list[str])->tuple[str,bytes]:
     else:
         return not_found(),b""
 
-    try:
-        with open(path, 'rb') as file_data:
-            data = file_data.read()
-    except Exception as e:
-        return server_error(e), b""
+    data:bytes
+    if (path not in file_map) or not (file_map[path].data):
+        try:
+            with open(path, 'rb') as FileData:
+                print("reading file:",path)
+                data = FileData.read()
+        except Exception as e:
+            return server_error(e), b""
+    else:
+        data = file_map[path].data
 
+    if path not in file_map:
+        file_map[path] = FileRecord(data,1)
+    else:
+        file_map[path].counter +=1
 
     response = "HTTP/1.1 200 Ok \r\n"+\
               f"Content-Type: {content_type}\r\n"+\
@@ -113,10 +158,16 @@ def main():
         server.bind(('0.0.0.0',8080))
         server.listen(8)
 
-        with ThreadPoolExecutor(max_workers=8) as pool:
+        if True:
             while True:
-                client,_ = server.accept()
-                pool.submit(handle_client,client)
+                client,addr = server.accept()
+                handle_client(client)
+        else:
+            with ThreadPoolExecutor(max_workers=8) as pool:
+                while True:
+                    client,addr = server.accept()
+                    _ = addr[1]
+                    pool.submit(handle_client,client)
 
 if __name__ == "__main__":
     main()
